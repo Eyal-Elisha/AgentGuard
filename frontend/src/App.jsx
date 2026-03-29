a57ba2119c9a9a476ea4b61e6f73688d02d4f0c9
ItayFrisk <itay.manevych@gmail.com>
2026-03-29 20:39:35 +0300

    Create rules dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import RulesDashboard from './components/RulesDashboard/RulesDashboard.jsx';
 import SessionsDashboard from './components/SessionsDashboard/SessionsDashboard.jsx';
 
 function App() {
-  return <SessionsDashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SessionsDashboard />} />
+        <Route path="/rules" element={<RulesDashboard />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
-
